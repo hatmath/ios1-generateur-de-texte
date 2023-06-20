de7b04d5d19b2c1c0c583af2bce45779bbf1fdce
Mathieu H <mathieuhatin@hotmail.com>
2023-06-20 03:58:20 -0700

    final ou presque

--- ios1-labo2/ViewController.swift
+++ ios1-labo2/ViewController.swift
@@ -12,25 +12,26 @@ class ViewController: UIViewController {
     //-- Outlets
     @IBOutlet weak var txtOutput: UILabel!
     @IBOutlet weak var btnGenerate: UIButton!
+    @IBOutlet weak var imgTornado: UIImageView!
     
     //-- Variables globales
     let reference = ["Tu est", "Ton oncle est"]
-    
     let celebrities = ["le Donald Trump", "la Tina Turner"]
-    
     let activities = ["de la pop", "de la finance"]
     
     
     //-- Fonctions
     @IBAction func btnGen(_ sender: Any) {
-       let referenceRandomNb = Int.random(in: 0...1)
-       let celebritiesRandomNb = Int.random(in: 0...1)
-       let activitiesRandomNb = Int.random(in: 0...1)
-        
-       txtOutput.text = reference[referenceRandomNb] + " "
+        txtOutput.textAlignment = .center
+        let referenceRandomNb = Int.random(in: 0...1)
+        let celebritiesRandomNb = Int.random(in: 0...1)
+        let activitiesRandomNb = Int.random(in: 0...1)
+    
+        imgTornado.isHidden = true
+        txtOutput.text = reference[referenceRandomNb] + " "
             + celebrities[celebritiesRandomNb] + " "
             + activities[activitiesRandomNb]
-    }
+   }
     
     override func viewDidLoad() {
         super.viewDidLoad()
